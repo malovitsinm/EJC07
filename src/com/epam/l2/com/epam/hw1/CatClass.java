9dfdc695c79247a053f64c7e5d87dae77f801764
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-10-05 20:18:22 +0400

    Added project for Java learning

---
+++ src/com/epam/l2/com/epam/hw1/CatClass.java
@@ -0,0 +1,24 @@
+package com.epam.l2.com.epam.hw1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public final class CatClass {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter number of Mississippi rivers:\n (More than 2, duh!)");
+        String param = reader.readLine();
+
+        int value = Integer.parseInt(param);
+
+        if (value > 2 && value < 153){
+            for (int i = 1; i < value + 1; i++) {
+                System.out.println("Mississippi " + i + "!");
+            }
+        }
+    }
+
+}
