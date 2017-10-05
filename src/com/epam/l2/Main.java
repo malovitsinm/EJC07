f63ad286522a131e422fabb79878cdcdb513d0c8
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-10-05 19:11:10 +0400

    Added project for Java learning

---
+++ src/com/epam/l2/Main.java
@@ -0,0 +1,34 @@
+package com.epam.l2;
+
+import java.nio.charset.CharsetEncoder;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println("Количество котов: "+ getCount(15));
+        System.out.println(main.getCharacter());
+    }
+
+    static public Long getCount(){
+        Long count = 10L;
+        long newCount = 10L;
+        return  count + newCount;
+    }
+
+    static public Long getCount(int size){
+        Long count = 10L;
+        long newCount = 10L;
+        if (size > 10) {
+            count *= 2;
+        }
+        return  count + newCount;
+    }
+
+    private Character getCharacter(){
+        Character character = new Character('C');
+        return  character;
+
+    }
+
+
+}
