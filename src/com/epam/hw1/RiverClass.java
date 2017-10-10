66466507c6f9852bf8279ae16db7f77dd2f64beb
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-10-10 20:46:06 +0400

    Add 1st homework + some cleaning up

--- src/com/epam/hw1/RiverClass.java
+++ src/com/epam/hw1/RiverClass.java
@@ -3,17 +3,18 @@ package com.epam.hw1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public final class RiverClass {
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please enter number of Mississippi rivers:\n (More than 2, duh!)");
+        System.out.println("Please enter a number of the Mississippi rivers:\n (More than 2, duh!)");
         String param = reader.readLine();
 
         int value = Integer.parseInt(param);
 
-        if (value > 2 && value < 153){
+        if (value > 2 && value < 153) {
             for (int i = 1; i < value + 1; i++) {
                 System.out.println("Mississippi " + i + "!");
             }
